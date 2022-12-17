c273842a657db9a63eb4053821e4180552ea2ab1
Daniel Esteban <decepeda@misena.edu.co>
2022-12-17 18:48:23 -0500

    Se crea el rest controller para category

--- src/main/java/com/danielesteban/inventory/controller/CategoryRestController.java
+++ src/main/java/com/danielesteban/inventory/controller/CategoryRestController.java
@@ -1,4 +1,23 @@
 package com.danielesteban.inventory.controller;
 
+import com.danielesteban.inventory.response.CategoryResponseRest;
+import com.danielesteban.inventory.services.ICategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1")
 public class CategoryRestController {
+
+    @Autowired
+    private ICategoryService service;
+
+    @GetMapping("/categories")
+    public ResponseEntity<CategoryResponseRest> searchCategories(){
+        ResponseEntity<CategoryResponseRest> response = service.search();
+        return response;
+    }
 }
