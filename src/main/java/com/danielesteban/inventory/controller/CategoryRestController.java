530bf3852f14c1a372500082d1906a151105da2a
Daniel Esteban <decepeda@misena.edu.co>
2023-01-28 19:13:42 -0500

    se configura el CORS

--- src/main/java/com/danielesteban/inventory/controller/CategoryRestController.java
+++ src/main/java/com/danielesteban/inventory/controller/CategoryRestController.java
@@ -6,6 +6,7 @@ import com.danielesteban.inventory.services.ICategoryService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin(origins = {"http://localhost:4200"})
 @RestController
 @RequestMapping("/api/v1")
 public class CategoryRestController {
