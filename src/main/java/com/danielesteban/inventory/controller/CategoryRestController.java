a194156bad0e332e18087f79ad33a2ea370d2f00
Daniel Esteban <decepeda@misena.edu.co>
2023-04-11 18:32:42 -0500

    Se crea funcion para exportar excel

--- src/main/java/com/danielesteban/inventory/controller/CategoryRestController.java
+++ src/main/java/com/danielesteban/inventory/controller/CategoryRestController.java
@@ -3,8 +3,13 @@ package com.danielesteban.inventory.controller;
 import com.danielesteban.inventory.model.Category;
 import com.danielesteban.inventory.response.CategoryResponseRest;
 import com.danielesteban.inventory.services.ICategoryService;
+import com.danielesteban.inventory.util.CategoryExcelExporter;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.io.IOException;
+import java.util.Objects;
 
 @CrossOrigin(origins = {"http://localhost:4200"})
 @RestController
@@ -38,5 +43,18 @@ public class CategoryRestController {
     @DeleteMapping("/categories/{id}")
     public ResponseEntity<?> delete(@PathVariable Long id){
         return service.deleteById(id);
+    }
+    @GetMapping("/categories/export/excel")
+    public  void exportToExcel(HttpServletResponse response) throws IOException{
+        response.setContentType("application/octet-stream");
+        String headerKey = "Content-Disposition";
+        String headerValue = "attachment; filename=result_category";
+        response.setHeader(headerKey, headerValue);
+
+        ResponseEntity<CategoryResponseRest> categoryResponse = service.search();
+        CategoryExcelExporter excelExporter;
+        excelExporter = new CategoryExcelExporter(Objects.requireNonNull(categoryResponse.getBody())
+                .getCategoryResponse().getCategories());
+        excelExporter.export(response);
     }
 }
